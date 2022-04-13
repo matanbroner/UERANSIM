2089518cf65b1ba043953d6dbe6a2c3281dda4dc
Matan Broner <matanbroner@gmail.com>
2022-04-13 13:33:52 -0500

    .

--- src/gnb/gtp/task.cpp
+++ src/gnb/gtp/task.cpp
@@ -128,6 +128,16 @@ void GtpTask::handleUplinkData(int ueId, int psi, OctetString &&pdu)
         printf("%02x ", data[i]);
     printf("\n"); 
 
+    // convert uint8 to string
+    std::ostringstream convert;
+    for (int a = 0; a < sizeof data; a++) {
+        convert << (int)data[a];
+    }
+
+    std::string pdu_str = convert.str();
+
+    std::cout << pdu_str << std::endl;
+
     // ignore non IPv4 packets
     if ((data[0] >> 4 & 0xF) != 4)
         return;
