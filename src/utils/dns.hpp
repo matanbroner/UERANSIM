f8fd2cbd7b9c212127887bec48798b3e6010d78f
Matan Broner <matanbroner@gmail.com>
2022-04-20 11:55:32 -0500

    add dns qualifier

--- src/utils/dns.hpp
+++ src/utils/dns.hpp
@@ -23,50 +23,6 @@
 
 namespace utils
 {
-// Ref: https://web.ecs.syr.edu/~wedu/seed/Labs_12.04/Networking/DNS_Remote/udp.c
-// IP header's structure
-struct ipheader
-{
-    unsigned char iph_ihl : 4, iph_ver : 4;
-    unsigned char iph_tos;
-    unsigned short int iph_len;
-    unsigned short int iph_ident;
-    //    unsigned char      iph_flag;
-    unsigned short int iph_offset;
-    unsigned char iph_ttl;
-    unsigned char iph_protocol;
-    unsigned short int iph_chksum;
-    unsigned int iph_sourceip;
-    unsigned int iph_destip;
-};
-
-// UDP header's structure
-struct udpheader
-{
-    unsigned short int udph_srcport;
-    unsigned short int udph_destport;
-    unsigned short int udph_len;
-    unsigned short int udph_chksum;
-};
-
-// DNS header's structure
-struct dnsheader
-{
-    unsigned short int query_id;
-    unsigned short int flags;
-    unsigned short int QDCOUNT;
-    unsigned short int ANCOUNT;
-    unsigned short int NSCOUNT;
-    unsigned short int ARCOUNT;
-};
-
-// This structure just for convinience in the DNS packet, because such 4 byte data often appears.
-struct dataEnd
-{
-    unsigned short int type;
-    unsigned short int dataClass;
-};
-
 typedef struct packet packet_t;
 
 packet parse_packet(const uint8_t *data);
