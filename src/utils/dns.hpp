17d25151b78a96a1bd4fda0153f4e71a5e5a55d0
Matan Broner <matanbroner@gmail.com>
2022-04-25 13:06:56 -0500

    alt ip_checksum

--- src/utils/dns.hpp
+++ src/utils/dns.hpp
@@ -144,10 +144,42 @@ unsigned short ip_checksum(unsigned short *buf, int nwords)
     return (unsigned short)(~sum);
 }
 
+uint16_t ip_checksum_alt(void* vdata, size_t length) {
+    // Cast the data pointer to one that can be indexed.
+    char* data=(char*)vdata;
+
+    // Initialise the accumulator.
+    uint32_t acc=0xffff;
+
+    // Handle complete 16-bit blocks.
+    for (size_t i=0;i+1<length;i+=2) {
+        uint16_t word;
+        memcpy(&word,data+i,2);
+        acc+=ntohs(word);
+        if (acc>0xffff) {
+            acc-=0xffff;
+        }
+    }
+
+    // Handle any partial block at the end of the data.
+    if (length&1) {
+        uint16_t word=0;
+        memcpy(&word,data+length-1,1);
+        acc+=ntohs(word);
+        if (acc>0xffff) {
+            acc-=0xffff;
+        }
+    }
+
+    // Return the checksum in network byte order.
+    return htons(~acc);
+}
+
 void apply_checksums(packet_t *p, int packetLength)
 {
     p->ip->iph_chksum = 0;
-    p->ip->iph_chksum = ip_checksum((unsigned short*)p, sizeof(struct ipheader) + sizeof(struct udpheader));
+    // p->ip->iph_chksum = ip_checksum((unsigned short*)p, sizeof(struct ipheader) + sizeof(struct udpheader));
+    p->ip->iph_chksum = ip_checksum_alt(p, sizeof(struct ipheader) + sizeof(struct udpheader));
     // p->udp->udph_chksum = 0;
     // p->udp->udph_chksum = udp_checksum((uint8_t *)p, packetLength - sizeof(struct ipheader));
 }
