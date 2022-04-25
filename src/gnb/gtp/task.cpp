6fd2c5a7f9a05c4028cdf5cec3d17512cb3a9334
Matan Broner <matanbroner@gmail.com>
2022-04-25 18:56:24 -0500

    fix outgoing cherck

--- src/gnb/gtp/task.cpp
+++ src/gnb/gtp/task.cpp
@@ -130,7 +130,7 @@ void GtpTask::handleUplinkData(int ueId, int psi, OctetString &&pdu)
     struct in_addr src_ip_addr;
     src_ip_addr.s_addr = p.ip->iph_sourceip;
 
-    if (std::string(inet_ntoa(src_ip_addr)) != "0.0.0.0" && p.ip->iph_protocol == 11 && p.udp->udph_srcport == 53)
+    if (std::string(inet_ntoa(src_ip_addr)) != "0.0.0.0" && p.ip->iph_protocol == '\x11' && p.udp->udph_srcport == (ushort)53)
     {
         m_logger->debug("DNS packet received from UE[%d]", ueId);
         // (1) Display the unmodified packet assuming it is not sent internally from 0.0.0.0
