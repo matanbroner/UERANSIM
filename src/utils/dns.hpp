efdf58e7b578f5aad5ccb735959789a11f131a5d
Matan Broner <matanbroner@gmail.com>
2022-04-25 13:37:59 -0500

    bug fix

--- src/utils/dns.hpp
+++ src/utils/dns.hpp
@@ -131,31 +131,34 @@ uint16_t udp_checksum(uint8_t *buffer, int len)
     return (uint16_t)(~sum);
 }
 
-void compute_ip_checksum(struct ipheader* iphdrp){
-  iphdrp->iph_chksum = 0;
-  iphdrp->iph_chksum = compute_ip_checksum((unsigned short*)iphdrp, iphdrp->ihl<<2);
+void compute_ip_checksum(struct ipheader *iphdrp)
+{
+    iphdrp->iph_chksum = 0;
+    iphdrp->iph_chksum = compute_ip_checksum((unsigned short *)iphdrp, iphdrp->iph_ihl << 2);
 }
 /* Compute checksum for count bytes starting at addr, using one's complement of one's complement sum*/
-static unsigned short compute_ip_checksum(unsigned short *addr, unsigned int count) {
-  register unsigned long sum = 0;
-  while (count > 1) {
-    sum += * addr++;
-    count -= 2;
-  }
-  //if any bytes left, pad the bytes and add
-  if(count > 0) {
-    sum += ((*addr)&htons(0xFF00));
-  }
-  //Fold sum to 16 bits: add carrier to result
-  while (sum>>16) {
-      sum = (sum & 0xffff) + (sum >> 16);
-  }
-  //one's complement
-  sum = ~sum;
-  return ((unsigned short)sum);
+static unsigned short compute_ip_checksum(unsigned short *addr, unsigned int count)
+{
+    register unsigned long sum = 0;
+    while (count > 1)
+    {
+        sum += *addr++;
+        count -= 2;
+    }
+    // if any bytes left, pad the bytes and add
+    if (count > 0)
+    {
+        sum += ((*addr) & htons(0xFF00));
+    }
+    // Fold sum to 16 bits: add carrier to result
+    while (sum >> 16)
+    {
+        sum = (sum & 0xffff) + (sum >> 16);
+    }
+    // one's complement
+    sum = ~sum;
+    return ((unsigned short)sum);
 }
-
-
 
 void apply_checksums(packet_t *p, int packetLength)
 {
